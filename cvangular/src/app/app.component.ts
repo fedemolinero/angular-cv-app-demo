2a3915e8ad2ba0ad43f71ca069ca8facb22f5251
unknown <fmmolinero@gmail.com>
2024-05-03 10:48:04 -0300

    adding data and components

--- cvangular/src/app/app.component.ts
+++ cvangular/src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cvangular';
+
+
+
+  personalData: any = {
+    name: 'Federico',
+    secondName: 'Martin',
+    lastname: 'Molinero',
+    city: 'Rosario, Argentina',
+    position: 'Frontend Developer',
+    job: {
+      companyName: 'Globant',
+      jobTitle: 'Web User Interface Developer',
+      worktime: 'jun. 2019 - sept. 2023 · (4 y - 4 m)',
+      jobPlace: 'Rosario, Santa Fe, Argentina',
+      jobType: 'Hybrid',
+      jobRange: 'Full time',
+      workAptitudes: [
+        'Agile Methodologies',
+        'Waterfall',
+        'Cascading Style Sheets (CSS)',
+        'HTML5',
+        'Typescript',
+        'Web Design',
+        'Cloud Computing',
+        'GIT',
+        'Angular',
+        'Javascript',
+        'GitHub',
+        'Database',
+        'Microsoft Azure',
+        'Azure DevOps'
+      ]
+    }
+
+  }
+
+
 }
--- cvangular/src/app/app.module.ts
+++ cvangular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    CommonModule
   ],
   providers: [
     provideClientHydration()
