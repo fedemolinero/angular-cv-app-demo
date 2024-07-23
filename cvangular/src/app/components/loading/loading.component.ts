050f93ada0f1ebc501c7c1547aedf361ab4be46c
unknown <fmmolinero@gmail.com>
2024-07-23 18:22:06 -0300

    upgrading loading

--- cvangular/src/app/components/loading/loading.component.ts
+++ cvangular/src/app/components/loading/loading.component.ts
@@ -2,11 +2,20 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
-  templateUrl: './loading.component.html',
+  template: `
+<div class="loading-spinner">
+  <div class="spinner-border text-white" role="status">
+    <span id="loading-status" class="visually-hidden"></span>
+  </div>
+  <span id="loading-message" class="p-3 m-3 text-white" *ngIf="message" aria-live="assertive">{{ message }}</span>
+  <span class="visually-hidden">Cargando...</span>
+</div>
+`,
   styleUrl: './loading.component.scss'
 })
 export class LoadingComponent {
 
   @Input() message!: string;
+  @Input() backgroundColor: string = '#0d6efd'; // Color por defecto
 
 }
