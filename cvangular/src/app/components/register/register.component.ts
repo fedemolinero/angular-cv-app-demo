f317144952185f573f7eb2a41b5dde8ffbe5d09c
unknown <fmmolinero@gmail.com>
2024-07-23 14:39:44 -0300

    adding code to register and login and some styles

--- cvangular/src/app/components/register/register.component.ts
+++ cvangular/src/app/components/register/register.component.ts
@@ -12,23 +12,25 @@ import { AuthService } from '@services/auth.service';
 
 export class RegisterComponent implements OnInit, OnDestroy {
 
-  private registerSubscription: Subscription | undefined;
-  registerForm: FormGroup;
-  isLoading: boolean = false;
-  errorMessage: string | undefined;
+  private registerSubscription!: Subscription;
+  registerForm!: FormGroup;
+
+  loading: boolean = false;
+
+  // Attempt control FE
+  maxAttempts: number = 3;
+  attempts: number = 0;
+  disabledRegister: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
-  ) {
-    this.registerForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-    });
-  }
+  ) { }
 
   ngOnInit(): void {
+    this.initregisterForm();
+    this.loadAttemptsFromStorage();
   }
 
   ngOnDestroy(): void {
@@ -37,33 +39,72 @@ export class RegisterComponent implements OnInit, OnDestroy {
     }
   }
 
+  initregisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+    });
+  }
+
+  loadAttemptsFromStorage() {
+    const storedAttempts = localStorage.getItem('loginAttempts');
+    if (storedAttempts) {
+      this.attempts = parseInt(storedAttempts, 10);
+    }
+  }
+
+  saveAttemptsToStorage() {
+    localStorage.setItem('loginAttempts', this.attempts.toString());
+  }
+
+  clearAttemptsFromStorage() {
+    localStorage.removeItem('loginAttempts');
+  }
+
   get username() { return this.registerForm.get('username'); }
   get password() { return this.registerForm.get('password'); }
 
   register() {
-    if (this.registerForm.invalid) {
+
+    if (this.disabledRegister || this.loading) {
       return;
     }
 
-    const username = this.username?.value;
-    const password = this.password?.value;
+    this.loading = true; // Establecer estado de carga
 
-    this.isLoading = true;
-    this.errorMessage = undefined;
+    if (this.attempts >= this.maxAttempts) {
+      this.disabledRegister = true;
+      setTimeout(() => {
+        this.attempts = 0;
+        this.disabledRegister = false;
+        this.clearAttemptsFromStorage(); // Limpiar intentos en el almacenamiento
+        this.loading = false; // Restaurar estado de carga
+      }, 60000); // Bloquea el inicio de sesión durante 1 minuto
+      return;
+    }
 
-    this.registerSubscription = this.authService.register(username, password)
-      .subscribe({
+    this.registerSubscription = this.authService.login(
+      this.registerForm.controls['username'].value,
+      this.registerForm.controls['password'].value
+    ).subscribe(
+      {
         next: (response: any) => {
           console.log('Registration successful', response);
-          this.isLoading = false;
+          this.loading = false;
           this.router.navigate(['/login']);
         },
-        error: (e) => {
-          console.error('Registration error', e);
-          this.errorMessage = 'Registration failed. Please try again.'; // Mostrar mensaje de error
-          this.isLoading = false;
-        }
-      });
-  }
+        error: (error) => {
 
+          // Si falla, incrementamos el contador de intentos
+          this.attempts++;
+          this.saveAttemptsToStorage(); // Guardar intentos en el almacenamiento
+          // Mostrar mensaje de contraseña incorrecta y enfocar el campo de password
+          if (error.status == 401) {
+            this.registerForm.controls['password'].setErrors({ 'is-invalid': true });
+          }
+          this.loading = false; // Restaurar estado de carga en caso de error
+        }
+      }
+    );
+  }
 }
